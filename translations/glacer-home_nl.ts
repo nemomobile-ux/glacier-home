58cbb8ee3308bb82baec00309fadcb7c0a5052f5
Bart Ribbers <bribbers@disroot.org>
2020-03-31 16:15:06 +0200

    [i18n] Add Dutch translations

---
+++ translations/glacer-home_nl.ts
@@ -0,0 +1,429 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AppSwitcher</name>
+    <message>
+        <location filename="../src/qml/AppSwitcher.qml" line="186"/>
+        <source>Done</source>
+        <translation>Klaar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppSwitcher.qml" line="202"/>
+        <source>Close All</source>
+        <translation>Alles sluiten</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppSwitcher.qml" line="212"/>
+        <source>Nothing open yet</source>
+        <translation>Nog niks open</translation>
+    </message>
+</context>
+<context>
+    <name>AudioWarningDialog</name>
+    <message>
+        <location filename="../src/qml/volumecontrol/AudioWarningDialog.qml" line="46"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/volumecontrol/AudioWarningDialog.qml" line="47"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/volumecontrol/AudioWarningDialog.qml" line="48"/>
+        <source>Dangerous volume level!</source>
+        <translation>Gevaarlijk geluidsniveau!</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/volumecontrol/AudioWarningDialog.qml" line="49"/>
+        <source>Do you want to continue?</source>
+        <translation>Wil je doorgaan?</translation>
+    </message>
+</context>
+<context>
+    <name>BtRequestConfirmationDialog</name>
+    <message>
+        <location filename="../src/qml/dialogs/BtRequestConfirmationDialog.qml" line="47"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/BtRequestConfirmationDialog.qml" line="48"/>
+        <source>Connect</source>
+        <translation>Verbinden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/BtRequestConfirmationDialog.qml" line="49"/>
+        <source>Connect to device </source>
+        <translation>Verbinden met apparaat</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/BtRequestConfirmationDialog.qml" line="50"/>
+        <source>code: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ControlCenter</name>
+    <message>
+        <location filename="../src/qml/ControlCenter.qml" line="122"/>
+        <source>Wi-Fi</source>
+        <translation>Wi-Fi</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ControlCenter.qml" line="127"/>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ControlCenter.qml" line="132"/>
+        <source>Data</source>
+        <translation>Data</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ControlCenter.qml" line="137"/>
+        <source>Location</source>
+        <translation>Locatie</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ControlCenter.qml" line="142"/>
+        <source>Quiet</source>
+        <translation>Stil</translation>
+    </message>
+</context>
+<context>
+    <name>Deleter</name>
+    <message>
+        <location filename="../src/qml/applauncher/Deleter.qml" line="38"/>
+        <source>Removing</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/Deleter.qml" line="40"/>
+        <source>Uninstalling</source>
+        <translation>De-installeren</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/Deleter.qml" line="61"/>
+        <location filename="../src/qml/applauncher/Deleter.qml" line="103"/>
+        <location filename="../src/qml/applauncher/Deleter.qml" line="181"/>
+        <source>Uninstall</source>
+        <translation>De-installeren</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/Deleter.qml" line="80"/>
+        <location filename="../src/qml/applauncher/Deleter.qml" line="93"/>
+        <location filename="../src/qml/applauncher/Deleter.qml" line="138"/>
+        <source>Remove</source>
+        <translation>Verwijderen</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceLockUI</name>
+    <message>
+        <location filename="../src/qml/lockscreen/DeviceLockUI.qml" line="160"/>
+        <source>Enter security code</source>
+        <translation>Voer de veiligheidscode in</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/lockscreen/DeviceLockUI.qml" line="164"/>
+        <source>Incorrect code</source>
+        <translation>Onjuiste code</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/lockscreen/DeviceLockUI.qml" line="172"/>
+        <source>Temporarily locked</source>
+        <translation>Tijdelijk vergrendeld</translation>
+    </message>
+</context>
+<context>
+    <name>FileUtils</name>
+    <message>
+        <location filename="../src/fileutils.cpp" line="44"/>
+        <source>Screenshot</source>
+        <translation>Schermafdruk</translation>
+    </message>
+</context>
+<context>
+    <name>LauncherItemWrapper</name>
+    <message>
+        <location filename="../src/qml/applauncher/LauncherItemWrapper.qml" line="187"/>
+        <source>folder</source>
+        <translation>map</translation>
+    </message>
+</context>
+<context>
+    <name>MediaController</name>
+    <message>
+        <location filename="../src/qml/statusbar/MediaController.qml" line="67"/>
+        <source>No music playing</source>
+        <translation>Er speelt geen muziek</translation>
+    </message>
+</context>
+<context>
+    <name>NotificationItem</name>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="67"/>
+        <source>years ago</source>
+        <translation>jaren geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="69"/>
+        <source>year ago</source>
+        <translation>een jaar geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="73"/>
+        <source>months ago</source>
+        <translation>maanden geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="75"/>
+        <source>month ago</source>
+        <translation>een maand geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="79"/>
+        <source>days ago</source>
+        <translation>dagen geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="81"/>
+        <source>day ago</source>
+        <translation>een dag geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="85"/>
+        <source>hours ago</source>
+        <translation>uren geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="87"/>
+        <source>hour ago</source>
+        <translation>een uur geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="91"/>
+        <source>minutes ago</source>
+        <translation>minuten geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="93"/>
+        <source>minute ago</source>
+        <translation>een minuut geleden</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/notifications/NotificationItem.qml" line="96"/>
+        <source>Just now</source>
+        <translation>Zojuist</translation>
+    </message>
+</context>
+<context>
+    <name>Screenshot</name>
+    <message>
+        <location filename="../src/qml/system/Screenshot.qml" line="67"/>
+        <source>Desktop</source>
+        <translation>Desktop</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/system/Screenshot.qml" line="68"/>
+        <source>Screenshot saved</source>
+        <translation>Schermafdruk opgeslagen</translation>
+    </message>
+</context>
+<context>
+    <name>SearchListView</name>
+    <message>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="122"/>
+        <source>Search</source>
+        <translation>Zoeken</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="148"/>
+        <source>Hide search panel</source>
+        <translation>Verberg zoekpaneel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="148"/>
+        <source>Pinup search panel</source>
+        <translation>Pin zoekpaneel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="196"/>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="273"/>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="283"/>
+        <source>Application</source>
+        <translation>Applicatie</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="198"/>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="294"/>
+        <source>Contact</source>
+        <translation>Contact</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="200"/>
+        <source>Content</source>
+        <translation>Inhoud</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="274"/>
+        <location filename="../src/qml/applauncher/SearchListView.qml" line="284"/>
+        <source>installed on you device</source>
+        <translation>geïnstalleerd op jouw apparaat</translation>
+    </message>
+</context>
+<context>
+    <name>USBModeSelector</name>
+    <message>
+        <location filename="../src/qml/connectivity/USBModeSelector.qml" line="75"/>
+        <source>Current mode: Charging only</source>
+        <translation>Huidige modus: alleen opladen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/connectivity/USBModeSelector.qml" line="91"/>
+        <source>MTP Mode</source>
+        <translation>MTP modus</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/connectivity/USBModeSelector.qml" line="111"/>
+        <source>Mass Storage Mode</source>
+        <translation>Massa opslag</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/connectivity/USBModeSelector.qml" line="131"/>
+        <source>Developer Mode</source>
+        <translation>Ontwikkelaarsmodus</translation>
+    </message>
+</context>
+<context>
+    <name>UsbModeDialog</name>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="50"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="51"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="52"/>
+        <source>Select USB mode</source>
+        <translation>Selecteer USB modus</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="68"/>
+        <source>Always ask</source>
+        <translation>Altijd vragen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="70"/>
+        <source>MTP</source>
+        <translation>MTP</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="72"/>
+        <source>Chagring only</source>
+        <translation>Alleen opladen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="74"/>
+        <source>Connection sharing</source>
+        <translation>Verbinding delen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="76"/>
+        <source>Developer mode</source>
+        <translation>Oantwikkelaarsmodus</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/dialogs/UsbModeDialog.qml" line="78"/>
+        <source>Busy</source>
+        <translation>Bezet</translation>
+    </message>
+</context>
+<context>
+    <name>WifiPanel</name>
+    <message>
+        <location filename="../src/qml/statusbar/WifiPanel.qml" line="41"/>
+        <source>Wifi</source>
+        <translation>Wifi</translation>
+    </message>
+</context>
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="../src/settings-plugins/desktop/desktop.qml" line="32"/>
+        <source>Desktop</source>
+        <translation>Desktop</translation>
+    </message>
+    <message>
+        <location filename="../src/settings-plugins/desktop/desktop.qml" line="58"/>
+        <source>Enable windowed mode</source>
+        <translation>Activeer venstermodus</translation>
+    </message>
+    <message>
+        <location filename="../src/settings-plugins/desktop/desktop.qml" line="87"/>
+        <source>Always show search panel</source>
+        <translation>Toon altijd het zoekpaneel</translation>
+    </message>
+</context>
+<context>
+    <name>notifications</name>
+    <message>
+        <location filename="../src/settings-plugins/notifications/notifications.qml" line="32"/>
+        <source>Notifications</source>
+        <translation>Notificaties</translation>
+    </message>
+    <message>
+        <location filename="../src/settings-plugins/notifications/notifications.qml" line="52"/>
+        <source>Show notification body on lockscreen</source>
+        <translation>Laat notificatie inhoud zien op het vergrendelscherm</translation>
+    </message>
+</context>
+<context>
+    <name>selectImage</name>
+    <message>
+        <location filename="../src/settings-plugins/wallpaper/selectImage.qml" line="33"/>
+        <source>Select wallpaper</source>
+        <translation>Selecteer achtergrond</translation>
+    </message>
+</context>
+<context>
+    <name>wallpaper</name>
+    <message>
+        <location filename="../src/settings-plugins/wallpaper/wallpaper.qml" line="32"/>
+        <source>Wallpapers</source>
+        <translation>Achtergronden</translation>
+    </message>
+    <message>
+        <location filename="../src/settings-plugins/wallpaper/wallpaper.qml" line="70"/>
+        <source>Use paralax effect for wallpaper</source>
+        <translation>Gebruik paralax effect</translation>
+    </message>
+    <message>
+        <location filename="../src/settings-plugins/wallpaper/wallpaper.qml" line="99"/>
+        <source>Use different images for lockscreen and home screen</source>
+        <translation>Gebruik een andere achtergrond voor het vergrendelscherm als het hoofdscherm</translation>
+    </message>
+    <message>
+        <location filename="../src/settings-plugins/wallpaper/wallpaper.qml" line="132"/>
+        <source>Homescreen wallpaper</source>
+        <translation>Hoofdscherm achtergrond</translation>
+    </message>
+    <message>
+        <location filename="../src/settings-plugins/wallpaper/wallpaper.qml" line="132"/>
+        <source>Wallpaper</source>
+        <translation>Achtergrond</translation>
+    </message>
+    <message>
+        <location filename="../src/settings-plugins/wallpaper/wallpaper.qml" line="161"/>
+        <source>Lockscreen wallpaper</source>
+        <translation>Vergrendelscherm achtergrond</translation>
+    </message>
+</context>
+</TS>
