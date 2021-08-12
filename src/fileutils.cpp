1cf5cc402292b31168739da8e5b7bf11b3ed99f6
Chupligin Sergey <neochapay@gmail.com>
2021-08-12 12:10:44 +0300

    [home] Use default menu order on first start

--- src/fileutils.cpp
+++ src/fileutils.cpp
@@ -18,17 +18,21 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
-// Copyright (c) 2020, Chupligin Sergey <neochapay@gmail.com>
+// Copyright (c) 2020-2021, Chupligin Sergey <neochapay@gmail.com>
 
 #include "fileutils.h"
 
 #include <QStandardPaths>
 #include <QDir>
+#include <QFile>
 #include <QDateTime>
+#include <QTextStream>
 
 FileUtils::FileUtils(QObject *parent) : QObject(parent)
 {
-
+    if(!QFile::exists(QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) + "/lipstick/applications.menu")) {
+        makeDefaultMenu();
+    }
 }
 
 QString FileUtils::getScreenshotPath()
@@ -44,4 +48,25 @@ QString FileUtils::getScreenshotPath()
     QString path = screenshotDir+"/"+tr("Screenshot")+"_"+QDateTime::currentDateTime().toString("ddMMyyyy-hhmmss")+".png";
 
     return path;
+}
+
+void FileUtils::makeDefaultMenu()
+{
+    QDir lipctickConfigDir(QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) + "/lipstick");
+    if(!lipctickConfigDir.exists()) {
+        lipctickConfigDir.mkpath(QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) + "/lipstick");
+    }
+
+    QFile menu(QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) + "/lipstick/applications.menu");
+    menu.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&menu);
+    out << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    out << "<Menu>\n";
+    out << "    <Filename>glacier-dialer.desktop</Filename>\n";
+    out << "    <Filename>glacier-messages.desktop</Filename>\n";
+    out << "    <Filename>glacier-contacts.desktop</Filename>\n";
+    out << "    <Filename>glacier-settings.desktop</Filename>\n";
+    out << "    <Filename>glacier-camera.desktop</Filename>\n";
+    out << "</Menu>\n";
+    menu.close();
 }
--- src/fileutils.h
+++ src/fileutils.h
@@ -10,6 +10,8 @@ public:
     explicit FileUtils(QObject *parent = 0);
 
     Q_INVOKABLE QString getScreenshotPath();
+private:
+    void makeDefaultMenu();
 
 };
 
