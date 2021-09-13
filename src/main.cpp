6453cd349e5fefb8266b5d4d50f9989beb32a07f
Chupligin Sergey <neochapay@gmail.com>
2021-09-13 18:25:19 +0300

    [compositor] Drop old unsupported API

--- src/main.cpp
+++ src/main.cpp
@@ -51,14 +51,7 @@ int main(int argc, char **argv)
 
     FileUtils *fileUtils = new FileUtils();
 
-    if (QT_VERSION_MAJOR == 5 && QT_VERSION_MINOR <= 7) {
-        app.setCompositorPath("/usr/share/lipstick-glacier-home-qt5/qml/compositor.qml");
-    }
-    if (QT_VERSION_MAJOR == 5 && QT_VERSION_MINOR > 7) {
-        app.setCompositorPath("/usr/share/lipstick-glacier-home-qt5/qml/compositor_new.qml");
-    }
-
-    //app.setCompositorPath("/usr/share/lipstick-glacier-home-qt5/qml/GlacierCompositor.qml");
+    app.setCompositorPath("/usr/share/lipstick-glacier-home-qt5/qml/GlacierCompositor.qml");
     Qt::ScreenOrientation nativeOrientation = app.primaryScreen()->nativeOrientation();
     QByteArray v = qgetenv("GLACIER_NATIVEORIENTATION");
     if (!v.isEmpty()) {
